7c573372c098771af881ea5e954fb5174e48bdb4
Nirmal S Samson <nirmalsamson2000@gmail.com>
2025-04-03 22:22:03 +0530

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home"
-import About from "./components/About"
-import Projects from "./components/Projects"
-import Skills from "./components/Skills"
-import Certificates from "./components/Certificates"
-import Contact from "./components/Contact"
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Certificates from "./components/Certificates";
+import Contact from "./components/Contact";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const API_URL = "https://nirmal-personal-portfolio.onrender.com";
-
 
   return (
     <>
-    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -24,12 +22,9 @@ function App() {
         <Route path="/skills" element={<Skills />} />
         <Route path="/certificates" element={<Certificates />} />
         <Route path="/contact" element={<Contact />} />
-    
       </Routes>
-    </Router>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
